d8dba7201bf8bbcf186cb6f763a357e6c9702303
Farhan Novyan Hafizh <64812051+farhan-hafizh@users.noreply.github.com>
2022-06-01 21:32:08 +0700

    commit

--- index.js
+++ index.js
@@ -2,21 +2,19 @@ var currentIndex=0
 showContent(0);
 
 function next(){
-    showContent(currentIndex+=1)
+    showContent(currentIndex+1)
 }
 
 function prev(){
-    showContent(currentIndex-=1)
+    showContent(currentIndex-1)
 }
 
 function showContent(index){
     var contents = document.getElementsByClassName("content");
-
+    contents[currentIndex].style.display="none";
     if(index<0) index=contents.length-1;
     else if (index>contents.length-1) index=0;
     currentIndex=index;
-    for(let i=0; i<contents.length; i++){
-        contents[i].style.display="none";
-    }
-    contents[index].style.display="block";
+    contents[currentIndex].style.display="block";
+   
 }
