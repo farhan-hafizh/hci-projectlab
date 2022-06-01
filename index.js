beef2e5f60d3b7d913fd243e06ae8df37491a20d
Farhan Novyan Hafizh <64812051+farhan-hafizh@users.noreply.github.com>
2022-06-01 22:32:19 +0700

    add gallery content(not finished)

--- index.js
+++ index.js
@@ -11,10 +11,14 @@ function prev(){
 
 function showContent(index){
     var contents = document.getElementsByClassName("content");
+    var thumbnails = document.getElementsByClassName("thumbnail");
     contents[currentIndex].style.display="none";
+    contents[currentIndex].classList.remove("fade-in");
+
     if(index<0) index=contents.length-1;
     else if (index>contents.length-1) index=0;
     currentIndex=index;
     contents[currentIndex].style.display="block";
+    contents[currentIndex].classList.add("fade-in");
    
 }
