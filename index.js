d48559e33192a8e82bcbccf7d147244d6640c61f
Farhan Novyan Hafizh <64812051+farhan-hafizh@users.noreply.github.com>
2022-06-02 16:39:25 +0700

    add gallery styling

--- index.js
+++ index.js
@@ -14,11 +14,17 @@ function showContent(index){
     var thumbnails = document.getElementsByClassName("thumbnail");
     contents[currentIndex].style.display="none";
     contents[currentIndex].classList.remove("fade-in");
-
+    
     if(index<0) index=contents.length-1;
     else if (index>contents.length-1) index=0;
+
+    if(thumbnails[index]){
+        thumbnails[currentIndex].classList.remove("thumb-active");
+        // thumbnails[currentIndex].style.opacity="50%";
+        // thumbnails[index].style.opacity="100%";
+        thumbnails[index].classList.add("thumb-active");
+    }
     currentIndex=index;
     contents[currentIndex].style.display="block";
-    contents[currentIndex].classList.add("fade-in");
    
 }
